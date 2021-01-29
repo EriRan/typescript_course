96c2eee230639b52b6b2acadbcbc549b1959ee53
Erik <erik.rantanen@hotmail.fi>
2021-01-29 20:10:00 +0200

    Add object, array and tuple sessions

--- app.ts
+++ app.ts
@@ -1,16 +1,54 @@
-function add(number1: number, number2: number) {
-  //Built in operator is from vanilla jacascript
-  console.log(typeof number1);
-  //WE could have a validation for the types in vanilla javascript but in typescript it is done automatically
-  //Javascript == dynamic types (resolved at runtime)
-  //Typescript == static types (set during development)
+//Object is one of the types
+//What is this
+//object does not support any variables
+//Curly braces after the variable mean a specialized object type where some info is provided of the type
+//We can define the variables inside the curly braces
+//You can define an object like this but its bad practice:
+/* const person: {
+  name: string;
+  age: number;
+}  */
+//Compilation removes the above!
 
-  //Primitive types in typescript are all lowercase
-  return number1 + number2;
+//THIS IS GOOD SYNTAX!
+const person: {
+  name: string;
+  nickname: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; //This is a tuple definition
+} = {
+  name: "BOB",
+  nickname: "LOMBOC",
+  age: 30,
+  hobbies: ["SPERTS", "Cooking"],
+  //This is a tuple. We need to explicitely overwrite the type. Otherwise it is considered an array
+  role: [2, "Author"],
+};
+
+let favoriteActivities: string[];
+//Mixed array not defined when defining like above
+favoriteActivities = ["SPORTS", "COOKEN"];
+
+//Do not use any array often
+//let anyArray: any[];
+
+//Cool missing variables are also detected
+console.log("My name is " + person.name);
+
+console.log("And I like: ");
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //We would get errors if the array had any content!
 }
 
-const number1 = 3;
-const number2 = 2.8;
+//Tuple type
+//Some programming languages have them, some don't
+//Array with two elements is not a tuple
 
-const result = add(number1, number2);
-console.log(result);
+//Why do these work? Ok I think I understand
+//This is a bit stupid though
+person.role.push("ADmen");
+person.role[1] = "asd";
+person.role[0] = 123;
+
