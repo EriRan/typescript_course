0660b527a6a2d52b5559ec66bd6bcb1ae561ef77
Erik <erik.rantanen@hotmail.fi>
2021-01-29 19:13:49 +0200

    Add some commented type training code

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log("COOOOOOL");
+function add(number1: number, number2: number) {
+  //Built in operator is from vanilla jacascript
+  console.log(typeof number1);
+  //WE could have a validation for the types in vanilla javascript but in typescript it is done automatically
+  //Javascript == dynamic types (resolved at runtime)
+  //Typescript == static types (set during development)
+
+  //Primitive types in typescript are all lowercase
+  return number1 + number2;
+}
+
+const number1 = 3;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
