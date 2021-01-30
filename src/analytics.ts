2770179fa202820b34514ef8566dd383c27a64cf
Erik <erik.rantanen@hotmail.fi>
2021-01-30 14:34:26 +0200

    Typescript and compiled js into different folders

---
+++ src/analytics.ts
@@ -0,0 +1,2 @@
+console.log("Analytics print...");
+//I'm going to need to git ignore the compiled javascript files somehow.
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+const userName = "Repe";
+console.log(userName);
+
+//Watch mode can target one file
+//Bigger projects must compile all the files ADDS
+
+const button = document.querySelector("button")!;
+
+button.addEventListener("click", () => {
+  console.log("Clicked!");
+});
+
+//Map needs to be added to lib
+const coolMap = new Map();
