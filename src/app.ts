3c24a8461f66311a16227a2e601894decafc0011
Erik <erik.rantanen@hotmail.fi>
2021-01-30 15:49:59 +0200

    Additional training material

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 const userName = "Repe";
 console.log(userName);
 
+function clickHandler(message: string) {
+  console.log("Clicked " + message);
+}
+
 //Watch mode can target one file
 //Bigger projects must compile all the files ADDS
 
-const button = document.querySelector("button")!;
+//Strict null checks setting would allow this without the exclamation mark
+const button = document.querySelector("button");
 
-button.addEventListener("click", () => {
-  console.log("Clicked MMM!");
-});
+//Better to do a null check here for extreme safety
+if (button) {
+  button.addEventListener("click", clickHandler.bind(null, "BUTTON!"));
+} else {
+  console.error("Button missing!")
+}
 
 //Map needs to be added to lib
 const coolMap = new Map();
--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,8 @@
-console.log("Analytics print...");
 //I'm going to need to git ignore the compiled javascript files somehow.
 //Haha this was called analytics.ts and that caused it to be ignored by addblock
+
+function sendAnalytics(data: string) {//Error here without string type definition with implicit any option as true
+  console.log("Analytics print: " + data);
+}
+
+sendAnalytics("COOLDATA");
