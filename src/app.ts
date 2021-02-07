01e961b17ccd92b4c0d71bb27db3bb030890b5b8
Erik <erik.rantanen@hotmail.fi>
2021-02-07 15:08:29 +0200

    Add user input handling and validation

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,62 @@
 //index.html and a css was provided. We need to write some code to make it all work
 //tsc -w == Quick way to run tsc --watch
 
+//Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+//This could be a lot better
+//MMMMMMMMMMMMMMMEga refactor!!!!!!
+//weeeee have no tests lol
+function validate(input: Validatable) {
+  let isValid = true;
+  if (input.required) {
+    isValid = isValid && input.value.toString().trim().length !== 0;
+  }
+  if (input.minLength != null && typeof input.value === "string") {
+    isValid = isValid && input.value.trim().length >= input.minLength;
+  }
+  if (input.maxLength != null && typeof input.value === "string") {
+    isValid = isValid && input.value.trim().length <= input.maxLength;
+  }
+  if (input.min != null && typeof input.value === "number") {
+    isValid = isValid && input.value >= input.min;
+  }
+  if (input.max != null && typeof input.value === "number") {
+    isValid = isValid && input.value <= input.max;
+  }
+  return isValid;
+}
+
+//Copied from git history hehe
+function Autobind(
+  _: any, //target, unused
+  _2: string | Symbol | number, //methodName, unused
+  descriptor: PropertyDescriptor
+) {
+  //Get the original method
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    //Extra logic when users try to access this property
+    //Getter is triggered by the concrete method
+    //This is some kind of extra layer
+    configurable: true,
+    //I guess enumberable means whether it can be iterated
+    enumerable: false,
+    get() {
+      const boundFunction = originalMethod.bind(this);
+      return boundFunction;
+    },
+  };
+  return adjustedDescriptor;
+}
+
 //Get access to the template form
 class ProjectInput {
   //This would be better to be called as formTemplate
@@ -11,6 +67,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -24,8 +83,79 @@ class ProjectInput {
       true
     );
     this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    //WHy is this not done in the html file....?
+    this.formElement.id = "user-input";
+
+    //Hashtag means ID
+    this.titleInputElement = this.formElement.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    this.configure();
     //Constructor does a call to attach! This is unique
     this.attach();
+  }
+
+  //We will return a tuple or nothing
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeopleCount = this.peopleInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleCountValidatable: Validatable = {
+      value: +enteredPeopleCount,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    //This is crap. Let's improve it in a later lecture
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleCountValidatable)
+    ) {
+      alert("Invalid input. Try again");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeopleCount];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+      this.clearInputs();
+    }
+  }
+
+  private configure() {
+    this.formElement.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
